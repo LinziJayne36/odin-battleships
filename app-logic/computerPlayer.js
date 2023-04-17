e362720dbe1c7f818956da4192047b1e0813896a
Lindsey W <lindsey.wilson1@outlook.com>
2023-04-17 16:54:22 +0100

    Extend the calcSelectedPositions method to allow for randomly plaing some ships horizontally

--- app-logic/computerPlayer.js
+++ app-logic/computerPlayer.js
@@ -16,7 +16,7 @@ export default class ComputerPlayer {
         const selectedPositions = [];
         const usedCoords = [];
 
-        const addShip = (length) => {
+        const addShip = (length, isVertical = true) => {
             const coords = [];
             let isValid = false;
             let iterations = 0;
@@ -29,21 +29,23 @@ export default class ComputerPlayer {
                     Math.floor(Math.random() * 10) + 1,
                 ];
 
-                // Check if any coordinate is out of bounds
+                if (!isVertical) {
+                    [firstCoord[0], firstCoord[1]] = [
+                        firstCoord[1],
+                        firstCoord[0],
+                    ];
+                }
+
                 if (firstCoord[0] > 10 || firstCoord[1] > 10) {
                     continue;
                 }
 
-                // Check if any coordinate is already used
                 let isOverlap = false;
                 for (let i = 0; i < length; i++) {
-                    const coordToCheck = [firstCoord[0] + i, firstCoord[1]];
+                    const coordToCheck = isVertical
+                        ? [firstCoord[0] + i, firstCoord[1]]
+                        : [firstCoord[0], firstCoord[1] + i];
                     if (usedCoords.includes(coordToCheck.toString())) {
-                        isOverlap = true;
-                        break;
-                    }
-                    const coordToCheck2 = [firstCoord[0], firstCoord[1] + i];
-                    if (usedCoords.includes(coordToCheck2.toString())) {
                         isOverlap = true;
                         break;
                     }
@@ -52,25 +54,15 @@ export default class ComputerPlayer {
                     continue;
                 }
 
-                // Check if all coordinates are valid
                 for (let i = 0; i < length; i++) {
-                    const coordToCheck = [firstCoord[0] + i, firstCoord[1]];
-                    if (
-                        coordToCheck[0] > 10 ||
-                        coordToCheck[1] > 10 ||
-                        coordToCheck[0] < 1 ||
-                        coordToCheck[1] < 1
-                    ) {
+                    const coordToAdd = isVertical
+                        ? [firstCoord[0] + i, firstCoord[1]]
+                        : [firstCoord[0], firstCoord[1] + i];
+                    if (coordToAdd[0] > 10 || coordToAdd[1] > 10) {
                         isValid = false;
                         break;
                     }
-                    const coordToCheck2 = [firstCoord[0], firstCoord[1] + i];
-                    if (
-                        coordToCheck2[0] > 10 ||
-                        coordToCheck2[1] > 10 ||
-                        coordToCheck2[0] < 1 ||
-                        coordToCheck2[1] < 1
-                    ) {
+                    if (usedCoords.includes(coordToAdd.toString())) {
                         isValid = false;
                         break;
                     }
@@ -78,7 +70,9 @@ export default class ComputerPlayer {
                 }
                 if (isValid) {
                     for (let i = 0; i < length; i++) {
-                        const coordToAdd = [firstCoord[0] + i, firstCoord[1]];
+                        const coordToAdd = isVertical
+                            ? [firstCoord[0] + i, firstCoord[1]]
+                            : [firstCoord[0], firstCoord[1] + i];
                         coords.push(coordToAdd);
                         usedCoords.push(coordToAdd.toString());
                     }
@@ -97,10 +91,10 @@ export default class ComputerPlayer {
             addShip(3);
         }
         for (let i = 0; i < 3; i++) {
-            addShip(2);
+            addShip(2, Math.random() < 0.5);
         }
         for (let i = 0; i < 4; i++) {
-            addShip(1);
+            addShip(1, Math.random() < 0.5);
         }
 
         this._selectedPositions = selectedPositions;
--- app-logic/player.js
+++ app-logic/player.js
@@ -27,7 +27,7 @@ export default class Player {
         const selectedPositions = [];
         const usedCoords = [];
 
-        const addShip = (length) => {
+        const addShip = (length, isVertical = true) => {
             const coords = [];
             let isValid = false;
             let iterations = 0;
@@ -40,21 +40,23 @@ export default class Player {
                     Math.floor(Math.random() * 10) + 1,
                 ];
 
-                // Check if any coordinate is out of bounds
+                if (!isVertical) {
+                    [firstCoord[0], firstCoord[1]] = [
+                        firstCoord[1],
+                        firstCoord[0],
+                    ];
+                }
+
                 if (firstCoord[0] > 10 || firstCoord[1] > 10) {
                     continue;
                 }
 
-                // Check if any coordinate is already used
                 let isOverlap = false;
                 for (let i = 0; i < length; i++) {
-                    const coordToCheck = [firstCoord[0] + i, firstCoord[1]];
+                    const coordToCheck = isVertical
+                        ? [firstCoord[0] + i, firstCoord[1]]
+                        : [firstCoord[0], firstCoord[1] + i];
                     if (usedCoords.includes(coordToCheck.toString())) {
-                        isOverlap = true;
-                        break;
-                    }
-                    const coordToCheck2 = [firstCoord[0], firstCoord[1] + i];
-                    if (usedCoords.includes(coordToCheck2.toString())) {
                         isOverlap = true;
                         break;
                     }
@@ -63,25 +65,15 @@ export default class Player {
                     continue;
                 }
 
-                // Check if all coordinates are valid
                 for (let i = 0; i < length; i++) {
-                    const coordToCheck = [firstCoord[0] + i, firstCoord[1]];
-                    if (
-                        coordToCheck[0] > 10 ||
-                        coordToCheck[1] > 10 ||
-                        coordToCheck[0] < 1 ||
-                        coordToCheck[1] < 1
-                    ) {
+                    const coordToAdd = isVertical
+                        ? [firstCoord[0] + i, firstCoord[1]]
+                        : [firstCoord[0], firstCoord[1] + i];
+                    if (coordToAdd[0] > 10 || coordToAdd[1] > 10) {
                         isValid = false;
                         break;
                     }
-                    const coordToCheck2 = [firstCoord[0], firstCoord[1] + i];
-                    if (
-                        coordToCheck2[0] > 10 ||
-                        coordToCheck2[1] > 10 ||
-                        coordToCheck2[0] < 1 ||
-                        coordToCheck2[1] < 1
-                    ) {
+                    if (usedCoords.includes(coordToAdd.toString())) {
                         isValid = false;
                         break;
                     }
@@ -89,7 +81,9 @@ export default class Player {
                 }
                 if (isValid) {
                     for (let i = 0; i < length; i++) {
-                        const coordToAdd = [firstCoord[0] + i, firstCoord[1]];
+                        const coordToAdd = isVertical
+                            ? [firstCoord[0] + i, firstCoord[1]]
+                            : [firstCoord[0], firstCoord[1] + i];
                         coords.push(coordToAdd);
                         usedCoords.push(coordToAdd.toString());
                     }
@@ -108,10 +102,10 @@ export default class Player {
             addShip(3);
         }
         for (let i = 0; i < 3; i++) {
-            addShip(2);
+            addShip(2, Math.random() < 0.5);
         }
         for (let i = 0; i < 4; i++) {
-            addShip(1);
+            addShip(1, Math.random() < 0.5);
         }
 
         this._selectedPositions = selectedPositions;
@@ -140,7 +134,7 @@ export default class Player {
         if (availableCells.length === 0) {
             // no available cells left, return null or throw an error
             //throw error.console("all the squares are taken");
-            return null;
+            return "full";
         }
         const randIndex = Math.floor(Math.random() * availableCells.length);
         const randCell = availableCells[randIndex];
